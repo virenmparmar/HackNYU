520ecea26f08c8fa4b84b5878f897e0cdba004c3
Viren Parmar <virenmparmar@gmail.com>
2023-02-18 18:42:14 -0500

    changed uploading to s3 structure

--- finder/findR/admin.py
+++ finder/findR/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import ItemListings, PictureDetails
+from .models import ItemListings
 # Register your models here.
 admin.site.register(ItemListings)
-admin.site.register(PictureDetails)
--- finder/findR/models.py
+++ finder/findR/models.py
@@ -8,12 +8,10 @@ from django.contrib.auth.models import User
 class ItemListings(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    photo_path = models.CharField(max_length=200)
+    picture = models.FileField(upload_to='media/%Y/%m/%d', blank=True)
     longitutude = models.FloatField()
     latitude = models.FloatField()
     status = models.CharField(max_length=1)
     rating = models.IntegerField()
 
-class PictureDetails(models.Model):
-    name = models.CharField(max_length=200)
-    picture = models.FileField(upload_to='media/')
+    
