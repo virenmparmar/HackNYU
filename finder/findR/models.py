20d544b07f30749ef07b298781061a326aaf009d
Charles Liu <cl5522@nyu.edu>
2023-02-19 02:16:13 -0500

    ADD added some functionality to image upload

--- finder/findR/models.py
+++ finder/findR/models.py
@@ -9,6 +9,7 @@ class ItemListings(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     photo_path = models.CharField(max_length=200)
+    image = models.FileField(upload_to="images/")
     longitutude = models.FloatField()
     latitude = models.FloatField()
     status = models.CharField(max_length=1)
--- finder/findR/views.py
+++ finder/findR/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 from .models import ItemListings, User
 # Create your views here.
+
+from django.contrib.auth.hashers import make_password
 
 def index(request):
     return render(request, "base.html")
@@ -25,7 +28,10 @@ def login(request):
         print(request.POST)
         try:
             user = User.objects.get(username=request.POST["username"])
-            if(user.check_password(request.POST["password"])):
+            print("hashed password is: ", make_password(request.POST["password"]))
+            print("password stored is: ", user.password)
+            if(user.check_password(make_password(request.POST["password"]))):
+                user.is_authenticated = True
                 return render(request, "add_listing.html")
             else:
                 message = "Invalid password"
@@ -37,25 +43,27 @@ def login(request):
         return render(request, "login.html")
 
 def add_listing(request):
-    if(request.method == "POST"):
-        try:
-            ItemListings.objects.create(
-            user_id=User.objects.get(username=request.POST["username"]),
-            photo_path=request.POST["photo_path"],
-            longitutude=request.POST["longitude"],
-            latitude=request.POST["latitude"],
-            status=request.POST["status"],
-            rating=request.POST["rating"],
-            )
-            message = "Listing added"
-            return render(request, "login.html", {"message": message})
-        except:
-            message = "Invalid username"
-            return render(request, "add_listing.html", {"message": message})
+    if (request.method == "POST" and request.FILES['post-photos']):
+        user = User.objects.get(username=request.user) 
+        upload = request.FILES['post-photos']       # Image object that was uploaded through the form
+        fss = FileSystemStorage()                   # File System Storage object
+        file = fss.save(upload.name, upload)        # Saving the image object using File System Storage object method
+        file_url = fss.url(file)                    # Finding the image url in file system
+        ItemListings.objects.create(
+        user_id=user,
+        photo_path=file_url,
+        image=upload,
+        longitutude=request.POST["post-longitude"],
+        latitude=request.POST["post-latitude"],
+        status='A',
+        rating=0,
+        )
+        message = "Listing added"
+        return render(request, "login.html", {"message": message, "file_url": file_url})
+    message = "Invalid username"
+    return render(request, "add_listing.html")
         
-    else:
-        return render(request, "add_listing.html")
-    
+
 def browse(request):
     if(request.method == "POST"):
         try:
